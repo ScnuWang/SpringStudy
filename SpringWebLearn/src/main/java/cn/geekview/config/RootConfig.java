91614bf38773f4820f082e9a22938c583990d999
ScnuWang <scnu_wang@163.com>
2017-10-19 17:57:04 +0800

    新增纯注解版的SpringMvc，直接使用Tomcat启动即可

---
+++ SpringWebLearn/src/main/java/cn/geekview/config/RootConfig.java
@@ -0,0 +1,14 @@
+package cn.geekview.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"cn.geekview"},excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION,value = EnableWebMvc.class)
+})
+public class RootConfig {
+}
---
+++ SpringWebLearn/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jason
+  Date: 2017/10/19
+  Time: 17:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spittr</title>
+</head>
+<body>
+Welcome to Spittr!
+<a href="/spittrs">Spittr列表</a>
+<a href="/spittr/register">注册</a>
+</body>
+</html>
